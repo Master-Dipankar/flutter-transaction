cf1a00a1b45e1e325296f3c8b35a7dd0a28ebe42
Dipankar Boruah☑️ <dipankarboruah3@gmail.com>
2023-03-05 02:04:14 +0530

    Final Commit

--- lib/pages/transaction.dart
+++ lib/pages/transaction.dart
@@ -63,10 +63,17 @@ class TransactionState extends State<Transaction> {
             width: MediaQuery.of(context).size.width * 1.0,
             height: MediaQuery.of(context).size.height * 0.2,
             ////////////////////////PART ONE STARTS///////////////////////////////////////////////////////
+<<<<<<< HEAD
             child: Row(
               children: [
                 Expanded(
                   flex: 2,
+=======
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 3,
+>>>>>>> 9664a00 (Initial commit! 2)
                   child: Container(
                     decoration: BoxDecoration(
                       color: Colors.white,
@@ -152,6 +159,7 @@ class TransactionState extends State<Transaction> {
                           ),
                         ),
 
+<<<<<<< HEAD
                         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                         // SizedBox(
                         //   width: MediaQuery.of(context).size.width * 0.8,
@@ -253,12 +261,16 @@ class TransactionState extends State<Transaction> {
                         //     ),
                         //   ),
                         // ),
+=======
+
+>>>>>>> 9664a00 (Initial commit! 2)
                       ],
                     ),
 
 
                   ),
                 ),
+<<<<<<< HEAD
 
 
             ////////////////////////////////////////PART ONE ENDS///////////////////////////////////////////////////////
@@ -297,17 +309,65 @@ class TransactionState extends State<Transaction> {
                             prefixIcon: Icon(Icons.search),
                             hintText: 'Search',
                             border: InputBorder.none,
+=======
+            ////////////////////////////////////////PART ONE ENDS///////////////////////////////////////////////////////
+          Padding(
+            padding: const EdgeInsets.all(100.0),
+            child: Expanded(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 4,
+                      blurRadius: 5,
+                      offset: const Offset(0, 2), // changes position of shadow
+                    ),
+                  ],
+                ),
+
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 14),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: MediaQuery.of(context).size.height * 0.05,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.search),
+                              hintText: 'Search',
+                              border: InputBorder.none,
+                            ),
+>>>>>>> 9664a00 (Initial commit! 2)
                           ),
                         ),
                       ),
                     ),
+<<<<<<< HEAD
                   ),
                 ],
+=======
+                  ],
+                ),
+>>>>>>> 9664a00 (Initial commit! 2)
               ),
             ),
           )
 
             ],
+<<<<<<< HEAD
             ),
           ),
         )
@@ -316,3 +376,120 @@ class TransactionState extends State<Transaction> {
     );
   }
 }
+=======
+
+
+        )
+
+
+              ),
+
+        ),
+    );
+  }
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SizedBox(
+//   width: MediaQuery.of(context).size.width * 0.8,
+//   height: MediaQuery.of(context).size.height * 0.05,
+//   child: ElevatedButton(
+//     onPressed: () {},
+//     style: ElevatedButton.styleFrom(
+//       backgroundColor: const Color(0xFF2F1256),
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.circular(10),
+//       ),
+//     ),
+//     child: Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: [
+//         const Text('Start Date'),
+//         const SizedBox(width: 2),
+//         SizedBox(
+//           width: MediaQuery.of(context).size.width * 0.3,
+//           height: MediaQuery.of(context).size.height * 0.05,
+//           child: TextFormField(
+//             keyboardType: TextInputType.datetime,
+//             inputFormatters: [
+//               MaskedTextInputFormatter(
+//                 mask: '##/##/##',
+//                 separator: '/',
+//               ),
+//             ],
+//             maxLength: 8,
+//             decoration: const InputDecoration(
+//               hintText: 'DD/MM/YY',
+//               hintStyle: TextStyle(fontSize: 18, color: Colors.grey),
+//               enabledBorder: UnderlineInputBorder(
+//                 borderSide: BorderSide(color: Colors.white),
+//               ),
+//               focusedBorder: UnderlineInputBorder(
+//                 borderSide: BorderSide(color: Colors.white),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ],
+//     ),
+//   ),
+// ),
+//////////////////////////////////////////////                  ////////////////////////////////////////////////////////////////////////////////////////////
+// SizedBox(
+//   width: MediaQuery.of(context).size.width * 0.8,
+//   height: MediaQuery.of(context).size.height * 0.05,
+//   child: ElevatedButton(
+//     onPressed: () {},
+//     style: ElevatedButton.styleFrom(
+//       backgroundColor: const Color(0xFF2F1256),
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.circular(10),
+//       ),
+//     ),
+//     child: Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: [
+//         const Text('Start Date'),
+//         const SizedBox(width: 2),
+//         SizedBox(
+//           width: MediaQuery.of(context).size.width * 0.3,
+//           height: MediaQuery.of(context).size.height * 0.05,
+//           child: TextFormField(
+//             keyboardType: TextInputType.datetime,
+//             maxLength: 8,
+//             controller: _dateEditingController,
+//             decoration: const InputDecoration(
+//               hintText: 'DD/MM/YY',
+//               hintStyle: TextStyle(fontSize: 18, color: Colors.grey),
+//               enabledBorder: UnderlineInputBorder(
+//                 borderSide: BorderSide(color: Colors.white),
+//               ),
+//               focusedBorder: UnderlineInputBorder(
+//                 borderSide: BorderSide(color: Colors.white),
+//               ),
+//             ),
+//             onChanged: (value) {
+//               String newValue = value.replaceAll("/", "");
+//               if (newValue.isNotEmpty && newValue.length < 3) {
+//                 newValue = "${newValue.substring(0, 2)}/${newValue.substring(2)}";
+//               } else if (newValue.length > 2 && newValue.length < 5) {
+//                 newValue = "${newValue.substring(0, 2)}/${newValue.substring(2, 4)}/${newValue.substring(4)}";
+//               }
+//               _dateEditingController.value = TextEditingValue(
+//                 text: newValue,
+//                 selection: TextSelection.fromPosition(
+//                   TextPosition(
+//                     offset: newValue.length,
+//                   ),
+//                 ),
+//               );
+//             },
+//           ),
+//         ),
+//       ],
+//     ),
+//   ),
+// ),
+>>>>>>> 9664a00 (Initial commit! 2)
