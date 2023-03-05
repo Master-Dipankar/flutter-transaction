214995543f90833872164a40429abeb065b3f0c5
Dipankar Boruah☑️ <dipankarboruah3@gmail.com>
2023-03-06 00:06:54 +0530

    Final responsive all working

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:transaction/pages/transaction.dart';
 import 'package:transaction/pages/transaction2.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           appBarTheme: const AppBarTheme(
-            backgroundColor:null,
-          )
-      ),
-      home:const Transaction2(),
+        backgroundColor: null,
+      )),
+      home: const Transaction2(),
     );
   }
 }
